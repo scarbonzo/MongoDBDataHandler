f793099835f84a71383b5eaa5a2145e72cafa26b
Rich Eodice <rich@eodice.com>
2018-01-11 13:45:47 -0600

    Added a Test/Console project

---
+++ MongoDBDataHandler/MongoDBDataHandler.Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MongoDBDataHandler.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoDBDataHandler db = new MongoDBDataHandler("localhost", "messaging");
+
+            Recipient r = new Recipient()
+            {
+                Name = "Richard Eodice",
+                Mobile = "7329215276",
+                Email = "rich@eodice.com",
+                Active = true
+            };
+
+            Console.WriteLine(db.WriteObject("recipients", r).ToString());
+
+            foreach(Recipient recipient in db.GetObjects<Recipient>("recipients"))
+            {
+                Console.WriteLine(recipient.Name + " - " + recipient.Mobile + " - " + recipient.Email);
+            }
+        }
+    }
+}
---
+++ MongoDBDataHandler/MongoDBDataHandler.Test/Recipient.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDBDataHandler.Test
+{
+    public class Recipient
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; }
+    }
+}
